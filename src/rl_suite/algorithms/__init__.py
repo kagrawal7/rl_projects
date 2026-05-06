099349debd24ef9e1eb695da709a93a0468e9dfb
kagrawal7 <kush.agr02@gmail.com>
2026-05-06 15:23:14 -0400

    factored out td code; more tweaks needed

--- src/rl_suite/algorithms/__init__.py
+++ src/rl_suite/algorithms/__init__.py
@@ -2,21 +2,18 @@
 
 from __future__ import annotations
 
-from .cartpole_td import AgentA
-from .cartpole_td import AgentB
-from .cartpole_td import AgentC
-from .cartpole_td import TDAgent
 from .gpi.gpi_algos.deterministic_gpi import DeterministicPI
 from .gpi.gpi_algos.stochastic_gpi import StochasticPI
 from .gpi.gpi_algos.value_iteration import ValueIterClass
 from .mc import OffPolicyMCAgent
+from .td import ExpectedSarsaAgent, QLearningAgent, SarsaAgent, TDAgent
 
 __all__ = [
-    "AgentA",
-    "AgentB",
-    "AgentC",
     "DeterministicPI",
+    "ExpectedSarsaAgent",
     "OffPolicyMCAgent",
+    "QLearningAgent",
+    "SarsaAgent",
     "StochasticPI",
     "TDAgent",
     "ValueIterClass",
